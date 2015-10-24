b637bfaa45f6c2f0dd2fc53cb52449ebe80e7884
David Davidson <g00271860@gmit.ie>
2015-10-24 01:37:33 +0100

    Make

--- gitpass.py
+++ gitpass.py
@@ -39,41 +39,41 @@ It returns the our_salt,gh,repo,branch objects. branch is set to master, just fu
     our_salt = os.urandom(16) # salty
     try:
         github_username = raw_input("Please input your github username: ").strip()
-    except Exception, e:
-        return False,False,False
+    except Exception, e: # here we have an exception. We should handle or log it.
+        return False,False,False # something else can bitch about this if it fucks up, I guess. XXX: Add error handling.
     try:
-        github_password = getpass.getpass("Please input your github password: ").strip() # we will change this to getpass.
-    except Exception, e:
-        return False,False,False
+        github_password = getpass.getpass("Please input your github password: ").strip() 
+    except Exception, e: # here we have an exception. We should handle or log it.
+        return False,False,False # something else can bitch about this if it fucks up, I guess. XXX: Add error handling.
     try:
         github_repo = raw_input("Please input your github repository: ").strip()
-    except Exception, e:
-        return False,False,False
+    except Exception, e: # here we have an exception. We should handle or log it.
+        return False,False,False # something else can bitch about this if it fucks up, I guess. XXX: Add error handling.
     config_data = {'salt': our_salt.encode('base64'),
                 'github_username': github_username,
                 'github_password': encrypt(master_password=master_password, our_salt=our_salt, data=github_password),
                 'github_repo': github_repo}
-    config_file = os.getenv("HOME")+"/.gitpass.conf"
+    config_file = os.getenv("HOME")+"/.gitpass.conf" # I have no idea if this works on Windows. Works on Linux just fine.
     with open(config_file, "w") as outfile:
-        json.dump(config_data, outfile)
+        json.dump(config_data, outfile) # theres gotta be a nicer way of doing this
     print "{+} Configuration file written!"
-    print "{*} Logging into Github now..."
+    print "{*} Logging into Github now..." # this whole section here needs error/exception handling and shit.
     gh,repo,branch = git_connect(github_username=github_username, github_password=github_password, github_repo=github_repo)
     print "{*} Creating blank password store..."
-    commit = repo.create_file("gitpass","initiate",base64.b64encode('{"creds":[]}'))# hack
-    return our_salt,gh,repo,branch
+    commit = repo.create_file("gitpass","initiate",base64.b64encode('{"creds":[]}'))# hack, to get around some stupid fucking errors
+    return our_salt,gh,repo,branch # these are useful objects
 
 def retrieve_config(master_password):
     """
 Reads in the config file stored in ~/.gitpass.conf, decrypts the github password, and returns the our_salt,gh,repo,branch objects for use.
     """
-    config_file = os.getenv("HOME")+"/.gitpass.conf"
-    configuration = json.loads(open(config_file, "rb").read())
-    our_salt = configuration['salt'].decode('base64')
-    github_username = configuration['github_username']
-    github_password = decrypt(master_password=master_password, our_salt=our_salt, data=configuration['github_password'])
-    github_repo = configuration['github_repo']
-    gh,repo,branch = git_connect(github_username=github_username, github_password=github_password, github_repo=github_repo)
+    config_file = os.getenv("HOME")+"/.gitpass.conf" # works on linux, fucked if I know about windows
+    configuration = json.loads(open(config_file, "rb").read()) # this works fine, but is sub optimal.
+    our_salt = configuration['salt'].decode('base64') # yeah, we had to encode the salt...
+    github_username = configuration['github_username'] # should we encrypt this for the craic? maybe
+    github_password = decrypt(master_password=master_password, our_salt=our_salt, data=configuration['github_password']) # death to plaintext
+    github_repo = configuration['github_repo'] # we might encrypt this too, for shits and giggles
+    gh,repo,branch = git_connect(github_username=github_username, github_password=github_password, github_repo=github_repo) # this needs exception handling
     return our_salt,gh,repo,branch
 
 def git_push(gh, repo, branch, data):
@@ -81,9 +81,9 @@ def git_push(gh, repo, branch, data):
 Pushes our data to the github repo. Stores it in a file named "gitpass", obviously. Returns True if success, or False if failure.
     """
     try:
-        commit = repo.contents(path="gitpass").update(message="lol", content=base64.b64encode(str(data)))
+        commit = repo.contents(path="gitpass").update(message="lol", content=base64.b64encode(str(data))) # fucking bodge
     except Exception, e:
-        print e
+        print e # we didn't handle it, we just print it and fucking move on. We should handle it.
         print "{!!} Oh no! Something wen't wrong! Try again later!"
 
 def git_pull(gh, repo, branch):
@@ -91,7 +91,7 @@ def git_pull(gh, repo, branch):
 Pulls our data from the github repo. Only looks for files named "gitpass" in "master". Returns data if success, or fails and some other fucking shit handles the error.
     """
     data = base64.b64decode(base64.b64decode(repo.contents(path="gitpass").content)) # kludge for another bug
-    return data.strip()
+    return data.strip() # bloody sneaky newlines
 
 def encrypt(master_password, our_salt, data):
     """
@@ -214,6 +214,8 @@ Simply reads in the password store, and lists them off like the following.
 [1] user: rick@astley.com site: https://twitter.com
 ...
 Basically, makes it easy to list your usernames.
+
+Later, we can add such nice things as "check the date and warn on 'expired' passwords!"
     """
     records = password_store['creds']
     for record in records:
@@ -237,6 +239,7 @@ update - update a password in the store.
 delete - delete a password from the store.
 pull - get the latest version of password store from git.
 commit - commit changes to the store to the git.
+quit - quits. I should probably have added this earlier.
     """
 
 def spawn_interactive_prompt(master_password, our_salt, gh, repo, branch):
@@ -249,7 +252,7 @@ This is the interactive "shell".
         try:
             command = raw_input("GitPass> ")
         except KeyboardInterrupt:
-            sys.exit("{!} Caught Ctrl+C, lets bail!")
+            sys.exit("\n{!} Caught Ctrl+C, lets bail!")
         if command == "help":
             print_help()
         if command == "list":
@@ -279,6 +282,8 @@ This is the interactive "shell".
             password_store = ast.literal_eval(git_pull(gh, repo, branch)) # this is definately unsafe.
         if command == "commit":
             git_push(gh, repo, branch, data=password_store)
+        if command == "quit":
+            sys.exit("Bye!")
 
 def main():
     if os.path.exists(os.getenv("HOME")+"/.gitpass.conf") != True:
@@ -296,3 +301,4 @@ if __name__ == "__main__":
     main()
 
 # holy fuck, am I seriously building this?
+# yes, yes you did. All because of a joke on irc.
